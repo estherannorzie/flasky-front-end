f806ca2da8502f64fbb425c3d0e8f7e95ad69427
Esther Annorzie <estherannorzie@gmail.com>
2022-06-19 18:33:32 -0400

    Created Character component

---
+++ components/Character.js
@@ -0,0 +1,15 @@
+import "../src/App.js";
+
+function Character() {
+  const chrom = {
+    age: 30,
+    nationality: "Ylisse",
+    hairColor: "Blue",
+    occupation: "Exalt",
+    isCool: true,
+  };
+
+  return <div></div>;
+}
+
+export default Character;
