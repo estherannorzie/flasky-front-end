df51e47a2a15d6bf5bba250c403422e0d98505fb
Esther Annorzie <estherannorzie@gmail.com>
2022-06-19 19:32:29 -0400

    Characters array holds characters, CharacterList returned

--- src/App.js
+++ src/App.js
@@ -4,10 +4,40 @@ import Character from "./components/Character.js";
 import CharacterList from "./components/CharacterList";
 
 function App() {
+  const characters = [
+    {
+      id: 1,
+      name: "Chrom",
+      age: 21,
+      nationality: "Ylisse",
+      hairColor: "Blue",
+      occupation: "Exalt of Ylisse",
+      alive: true,
+    },
+    {
+      id: 2,
+      name: "Emmeryn",
+      age: 27,
+      nationality: "Ylisse",
+      hairColor: "Blonde",
+      occupation: "Exalt of Ylisse",
+      alive: false,
+    },
+    {
+      id: 3,
+      name: "Lissa",
+      age: 17,
+      nationality: "Ylisse",
+      hairColor: "Blonde",
+      occupation: "Princess of Ylisse",
+      alive: true,
+    }
+  ];
+  
   return (
     <div>
       <h1>Video Game Characters</h1>
-      <CharacterList />
+      <CharacterList characters={characters}/>
     </div>
   );
 }
