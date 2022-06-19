6ff2568fcfe6d784d822e72d629373510c76ea39
Esther Annorzie <estherannorzie@gmail.com>
2022-06-19 19:19:15 -0400

    Removed Character instances, imported CharacterList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import "./components/Character.js";
 import Character from "./components/Character.js";
+import CharacterList from "./components/CharacterList";
 
 function App() {
   return (
     <div>
       <h1>Video Game Characters</h1>
-      <Character />
-      <Character />
-      <Character />
+      <CharacterList />
     </div>
   );
 }
