b8e9998e9a6ac94243392d5bf189173a6fc38a06
Esther Annorzie <estherannorzie@gmail.com>
2022-06-22 18:22:32 -0400

    Input added to change hair color

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,18 +11,25 @@ const Character = (props) => {
     isAlive ? setIsAlive(false) : setIsAlive(true);
   };
 
+  const [hairColor, setHairColor] = useState(props.hairColor);
+  // callback function
+  const changeHairColor = (event) => {
+    setHairColor(event.target.value);
+  };
+
   return (
     <div>
       <h2 className="character__name">Name: {props.name}</h2>
       <ul>
+        {/* props are removed for hairColor and life status since it's already defined in state */}
         <li>Age: {props.age}</li>
         <li>Nationality: {props.nationality}</li>
-        <li>Hair color: {props.hairColor}</li>
+        <li>Hair color: {hairColor}</li>
         <li>Occupation: {props.occupation}</li>
-        {/* removed props from link 18 so it could match the state in line 8 */}
         <li>Are they alive?: {isAlive ? "Yes" : "No"}</li>
       </ul>
       <button onClick={toggleLifeStatus}>Toggle Life Status</button>
+      <input type="text" value={hairColor} onChange={changeHairColor}></input>
     </div>
   );
 };
